0366a68c8e3ce56ab59bdb2bc431c69193a537a1
Michael A <nomicflux@gmail.com>
2018-07-21 22:00:53 -0500

    Uses Padic representation for vector

--- src/PadicVector.purs
+++ src/PadicVector.purs
@@ -5,43 +5,42 @@ import Data.Int (pow, toNumber)
 import Data.List (List)
 import Data.List as L
 import Data.Maybe (Maybe(..))
-import Data.Ord ((>))
+import Data.Ord ((<), (>), (>=))
 import Data.Tuple (Tuple(..))
 import HalogenHelpers.Coordinates (Coordinates)
 import Math as Math
 import PolarCoordinates (PolarCoordinates, mkPolar, polarToCartesian)
-import Prelude (mod, otherwise, ($), (*), (+), (-), (/))
+import Prelude (mod, otherwise, ($), (*), (+), (-), (/), (<=))
+
+padicRep :: Int -> Int -> List Int
+padicRep p x = go x
+ where
+   go y
+     | y == 0 = L.Nil
+     | otherwise =
+       let m = y `mod` p
+       in L.Cons m (go ((y - m) / p))
 
 addCoords :: Coordinates -> Coordinates -> Coordinates
 addCoords a b = { x: a.x + b.x
                 , y: a.y + b.y
                 }
 
-calcStep :: Int -> Int -> Int -> Tuple PolarCoordinates (Maybe Int)
-calcStep p n x =
-  let power = p `pow` n
-      pow_1 = toNumber $ p `pow` (n - 1)
-      m = x `mod` power
-      r = 1.0 / pow_1
-      theta = Math.pi * 2.0 / toNumber power * toNumber m
-      pc = mkPolar r theta
-  in if power > x
-     then Tuple pc Nothing
-     else Tuple pc (Just $ x - m)
+calcStep :: Int -> Int -> Int -> PolarCoordinates
+calcStep p n d =
+  let r = 1.0 / toNumber (p `pow` n)
+      theta = Math.tau / toNumber p * toNumber d
+  in mkPolar r theta
 
-toCoordList :: Int -> Int -> Int -> List PolarCoordinates
-toCoordList p n x =
-  let (Tuple pc next) = calcStep p n x
-  in case next of
-    Nothing -> L.Cons pc L.Nil
-    Just y -> L.Cons pc (toCoordList p (n + 1) y)
+toCoordList :: Int -> Int -> List PolarCoordinates
+toCoordList p x =
+  let padic = padicRep p x
+  in L.mapWithIndex (calcStep p) padic
 
 baseCoordinates :: Coordinates
 baseCoordinates = {x: 0.0, y: 0.0}
 
 toVector :: Int -> Int -> Coordinates
-toVector p x
-  | x == 0 = baseCoordinates
-  | otherwise =
-    let cl = toCoordList p 1 x
-    in L.foldl (\acc next -> addCoords acc (polarToCartesian next)) baseCoordinates cl
+toVector p x =
+  let cl = toCoordList p x
+  in L.foldl (\acc next -> addCoords acc (polarToCartesian next)) baseCoordinates cl
